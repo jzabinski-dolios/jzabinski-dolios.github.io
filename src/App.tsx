5730b6fef9834115e8723e595fde82aea2475ed2
JOSEPH ZABINSKI <jzabinski@dolios.dev>
2023-08-30 17:55:21 -0400

    Replace css with scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import { ReactElement } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App(): ReactElement {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <p id="motto">Ubiquiti FE Test Assignment</p>
         <a
           className="App-link"
           href="https://reactjs.org"
