1cda44cd1b357e95da8c9a52a138a4d7c6dba462
JOSEPH ZABINSKI <jzabinski@dolios.dev>
2023-08-31 18:33:02 -0400

    Navigate to devices automatically

--- src/pages/Splash.tsx
+++ src/pages/Splash.tsx
@@ -1,10 +1,17 @@
-import { ReactElement } from 'react';
+import { ReactElement, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { ReactComponent as UbiquitiIcon } from '../ubi.svg';
 import './Splash.scss';
 
-export const Splash = (): ReactElement => (
-  <div className="Splash">
-    <UbiquitiIcon id="icon" />
-    <p id="motto">Ubiquiti FE Test Assignment</p>
-  </div>
-);
+export const Splash = (): ReactElement => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    setTimeout(() => navigate('./devices'), 1000);
+  }, [navigate]);
+  return (
+    <div className="Splash">
+      <UbiquitiIcon id="icon" />
+      <p id="motto">Ubiquiti FE Test Assignment</p>
+    </div>
+  );
+};
