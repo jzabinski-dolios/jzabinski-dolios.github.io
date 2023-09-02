664e0ddc722068a89dc10ac0db1c1076e29a06b7
JOSEPH ZABINSKI <jzabinski@dolios.dev>
2023-09-02 13:20:24 -0400

    Close results on blur

--- src/pages/devices/DevicesSearch.tsx
+++ src/pages/devices/DevicesSearch.tsx
@@ -1,14 +1,19 @@
 import { useSearchParams } from 'react-router-dom';
 import { ReactComponent as SearchIcon } from '../../icons/search.svg';
-import { ChangeEvent, ReactElement } from 'react';
+import { ChangeEvent, ReactElement, useState } from 'react';
 import { DevicesSearchResults } from './DevicesSearchResults';
 import './DevicesSearch.scss';
 
 export const DevicesSearch = (): ReactElement => {
-  const [searchParams, setSearchParams] = useSearchParams();
+  const [, setSearchParams] = useSearchParams();
+  const [blurred, setBlurred] = useState(false);
   return (
     <>
-      <div className="devices-search">
+      <div
+        className="devices-search"
+        onBlur={(e) => setBlurred(true)}
+        onFocus={() => setBlurred(false)}
+      >
         <div className="devices-search-layout">
           <div className="devices-search-attribs">
             <div className="devices-search-attribs-icon">
@@ -24,7 +29,11 @@ export const DevicesSearch = (): ReactElement => {
             />
           </div>
         </div>
-        <DevicesSearchResults />
+        {!blurred && (
+          <>
+            <DevicesSearchResults />
+          </>
+        )}
       </div>
     </>
   );
