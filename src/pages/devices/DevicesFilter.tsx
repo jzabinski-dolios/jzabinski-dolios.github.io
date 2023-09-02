76a13f1576499493ec6fbb63847b6d46d365e57d
JOSEPH ZABINSKI <jzabinski@dolios.dev>
2023-09-02 16:26:46 -0400

    Filter choices

--- src/pages/devices/DevicesFilter.tsx
+++ src/pages/devices/DevicesFilter.tsx
@@ -1,8 +1,15 @@
 import { ReactElement, useState } from 'react';
+import { deviceList } from '../../uidb';
 import './DevicesFilter.scss';
 
 export const DevicesFilter = (): ReactElement => {
-  const [blurred, setBlurred] = useState(false);
+  const lines = deviceList.devices.reduce<Array<string>>((uniqueLines, currDevice) => {
+    if (currDevice.line?.name && !uniqueLines.includes(currDevice.line.name)) {
+      uniqueLines.push(currDevice.line.name);
+    }
+    return uniqueLines;
+  }, []);
+  const [blurred, setBlurred] = useState(true);
   return (
     <>
       <div className="device-search-filter-ctr">
@@ -18,12 +25,30 @@ export const DevicesFilter = (): ReactElement => {
           onFocus={() => setBlurred(false)}
         >
           Filter
+          {!blurred && (
+            // Choices need to be mounted as children of the button so that blurring works well.
+            // position:fixed helps the choices to display outside the button.
+            <>
+              <div className="devices-search-filter-options">
+                <div className="devices-search-filter-options-title">Product line</div>
+                {lines.length > 0 && (
+                  <>
+                    <div className="device-search-filter-options-choices">
+                      {lines.map((line) => (
+                        <>
+                          <div className="device-search-filter-options-choice">
+                            <input type="checkbox" value={line} name={line} />
+                            <label htmlFor={line}>{line}</label>
+                          </div>
+                        </>
+                      ))}
+                    </div>
+                  </>
+                )}
+              </div>
+            </>
+          )}
         </button>
-        {!blurred && (
-          <>
-            <div className="devices-search-filter-options">123</div>
-          </>
-        )}
       </div>
     </>
   );
