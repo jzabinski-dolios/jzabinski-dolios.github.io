3e47f3b095c23b5627f047794cbb16c38bab7e43
JOSEPH ZABINSKI <jzabinski@dolios.dev>
2023-09-03 19:27:08 -0400

    Move functions in common to new file

--- src/pages/devices/DevicesGrid.tsx
+++ src/pages/devices/DevicesGrid.tsx
@@ -3,47 +3,18 @@ import { deviceList } from '../../uidb';
 import './DevicesGrid.scss';
 import { useNavigate, useSearchParams } from 'react-router-dom';
 import { DevicesSearchParams } from '../../Routes';
+import { ProductTblData, findLeastResolution } from './devicesUtilities';
 
-interface ProductTblData {
-  id: string;
-  line: string;
-  description: string;
-  image: {
-    iconID: string;
-    resolution: [number, number] | null;
-  };
+interface ProductTblGridData extends ProductTblData {
   shortnames: Array<string>;
 }
-
-const findLeastResolution = (
-  resolutions: Array<[number, number]>,
-  desiredResolution: number
-): [number, number] => {
-  // Spec says that the images 'should' all be square; remove any that are not, just in case.
-  const squareRes = resolutions.filter(([width, height]) => width === height);
-  // Sort by width, in case they are not yet sorted.
-  const sorted = squareRes.sort(
-    ([widthA], [widthB]) =>
-      Math.abs(desiredResolution - widthA) - Math.abs(desiredResolution - widthB)
-  );
-  // The desired resolution is the smallest possible (for efficient bandwidth),
-  // which is either slightly bigger or equal to the requested resolution (for image quality).
-  // (Slightly bigger resolutions will be shrunk down well. Smaller resolutions will be potentially grainy.)
-  const onlyEqualOrMore = sorted.filter(([width]) => width >= desiredResolution);
-  // Send the smallest resolution set if available. If nothing was equal to or bigger than the desired resolution,
-  // send the biggest one available. (For example, if we want 25 pixels and we have resolutions of 10 and 19 px, use 19.)
-  if (onlyEqualOrMore.length > 0) {
-    return onlyEqualOrMore[0];
-  }
-  return sorted[sorted.length - 1];
-};
 
 export const DevicesGrid = (): ReactElement => {
   const [searchParams, setSearchParams] = useSearchParams();
   const DEFAULT_RES = 84;
   const rawFilters = searchParams.get('filters');
   const filters = rawFilters?.split(',') ?? [];
-  const products = deviceList.devices.reduce<Array<ProductTblData>>((fullList, currDevice) => {
+  const products = deviceList.devices.reduce<Array<ProductTblGridData>>((fullList, currDevice) => {
     const line = currDevice.line?.name;
     if (line && filters.includes(line)) {
       return fullList;
